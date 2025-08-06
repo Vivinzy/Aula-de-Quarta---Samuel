8e082d207caf048b08288a6fbf061c0ff6e2e3f6
Vivinzy <vitorianunespera@gmail.com>
2025-08-06 15:44:45 -0300

    Aula 06.08

---
+++ Aula 02/IfElse.php
@@ -0,0 +1,12 @@
+<?php
+
+$nota1 = 8.5;
+$nota2 = 6.0;
+$media = ($nota1 + $nota2) / 2;
+
+if ($media >= 7) {
+    echo "Aprovado com média: $media";
+} else {
+    echo "Reprovado com média: $media";
+}
+?>
